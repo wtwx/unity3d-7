cece899bd50c76c43ba14a50c5bc1c4c6184b5b5
yhl123 <15301162@bjtu.edu.cn>
2018-04-18 10:29:28 +0800

    solider_stand

--- NEWshoot/Assets/Standard Assets/models/SkeletalMesh/Move.cs
+++ NEWshoot/Assets/Standard Assets/models/SkeletalMesh/Move.cs
@@ -1,6 +1,5 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using System.Collections;
 
 public class Move : MonoBehaviour {
     private float speed = 0.0f;
@@ -10,33 +9,36 @@ public class Move : MonoBehaviour {
         GetComponent<Animation>()["Run"].wrapMode = WrapMode.Loop;
         GetComponent<Animation>()["StandingFire"].wrapMode = WrapMode.Loop;
         GetComponent<Animation>()["RunFire"].wrapMode = WrapMode.Loop;
+        GetComponent<Animation>()["Crouch"].wrapMode = WrapMode.Loop;
         GetComponent<Animation>()["RunJump"].wrapMode = WrapMode.Once;
 
         GetComponent<Animation>()["Standing"].layer = 1;
         GetComponent<Animation>()["Run"].layer = 1;
         GetComponent<Animation>()["StandingFire"].layer = 1;
         GetComponent<Animation>()["RunFire"].layer = 1;
+        GetComponent<Animation>()["Crouch"].layer = 1;
         GetComponent<Animation>()["RunJump"].layer = 2;
 
         GetComponent<Animation>()["RunJump"].speed = 3;
+	
 	}
 	
 	// Update is called once per frame
 	void Update () {
         this.transform.Translate(0, 0, speed);
-		if(Input.GetKey(KeyCode.W))
+        if (Input.GetKey(KeyCode.W))
         {
             speed = 0.15f;
             GetComponent<Animation>().CrossFade("Run",0.3f);
         }
-        else if(Input.GetKeyUp(KeyCode.W))
+        else if (Input.GetKeyUp(KeyCode.W))
         {
             speed = 0.0f;
         }
 
-        if(speed==0.0f)
+        if (speed == 0.0f)
         {
-            GetComponent<Animation>().CrossFade("Standing", 0.3f);
+            GetComponent<Animation>().CrossFade("Standing",0.3f);
         }
 
         if (Input.GetKey(KeyCode.A))
@@ -50,7 +52,7 @@ public class Move : MonoBehaviour {
 
         if (Input.GetKey(KeyCode.Mouse0))
         {
-            if(speed==0.0f)
+            if (speed == 0.0f)
             {
                 GetComponent<Animation>().CrossFade("StandingFire", 0.1f);
             }
@@ -63,6 +65,12 @@ public class Move : MonoBehaviour {
         if (Input.GetKeyDown(KeyCode.Space))
         {
             GetComponent<Animation>().CrossFade("RunJump", 0.1f);
-         }
+        }
+
+        if (Input.GetKey(KeyCode.Z))
+        {
+            GetComponent<Animation>().CrossFade("Crouch", 0.2f);
+        }
+	
 	}
 }
--- NEWshoot/Assets/Standard Assets/models/SoundCtr.cs
+++ NEWshoot/Assets/Standard Assets/models/SoundCtr.cs
@@ -1,17 +1,17 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using System.Collections;
 
 public class SoundCtr : MonoBehaviour {
+
     public AudioClip aSound;
 	// Use this for initialization
 	void Start () {
-		
+	
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+	
 	}
 
     void PlaySound()
